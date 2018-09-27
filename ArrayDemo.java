9e944ac4785dab51953211994d8fefae350ad8af
Aditi Haiman <aditi@Aditis-MacBook-Air.local>
2018-09-27 17:30:00 -0400

    fixed problem 2b and tested it

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -3,33 +3,33 @@
 public class ArrayDemo {
   public static void main(String[] args) { //tests for all solutions
     int[] ary = {1, 2, 3, 4};
-    print1DArray(ary); //returns [1, 2, 3, 4]
+    printArray(ary); //returns [1, 2, 3, 4]
     int[] ary2 = {-1, 0, };
-    print1DArray(ary2); //returns [-1, 0]
+    printArray(ary2); //returns [-1, 0]
     int[] ary3 = {};
-    print1DArray(ary3); //returns []
+    printArray(ary3); //returns []
 
     int[][] arry = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}; // returns [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-    print2DArray(arry);
+    printArray(arry);
     int[][] arry2 = {{}, {}, {1, 1}}; // returns [[], [], [1, 1]]
-    print2DArray(arry2);
+    printArray(arry2);
 
     int[][] array1 = {{0, 0}, {}, {1, 2}, {1, 2, 0}}; //returns 3
     System.out.println(countZeros2D(array1));
     int[][] array2 = {{30}, {3}, {1}, {1, 2}}; //returns 0
     System.out.println(countZeros2D(array2));
 
-    int[][] arr = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}}; //returns {{3, 1, 1}, {1, 3, 1}, {1, 1, 3}}
+    int[][] arr = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}}; //returns [[3, 1, 1], [1, 3, 1], [1, 1, 3]]
     fill2D(arr);
-    int[][] arr2 = {{1, 1, 1, 1}, {0}, {0, 0}}; //returns {{3, 1, 1, 1}, {1}, {1, 1}
+    int[][] arr2 = {{1, 1, 1, 1}, {0}, {0, 0}}; //returns [[3, 1, 1, 1], [1], [1, 1]]
     fill2D(arr2);
 
-    int[][] arr3 = {{1, 1, 1, 1}, {0}, {0, 0}};
+    int[][] arr3 = {{3, 4, 5}, {-1}, {}}; //returns [[1, 1, 1], [3], []]
     fill2DCopy(arr3);
 
   }
 
-  public static void print1DArray(int[] ary) { //0a. Make a function to print a 1D array of ints.
+  public static void printArray(int[] ary) { //0a. Make a function to print a 1D array of ints.
     System.out.print("[");
     for (int x = 0; x < ary.length; x++) {
       if (x == ary.length - 1) {
@@ -42,22 +42,22 @@ public class ArrayDemo {
     System.out.print("]\n");
   }
 
-  public static void print2DArray(int[][]arry) { //0b. Make a function to print a 2d array of ints.
+  public static void printArray(int[][]ary) { //0b. Make a function to print a 2d array of ints.
     System.out.print("[[");
     String tempOutput = "";
-    for (int x = 0; x < arry.length; x++) {
-      if (arry[x].length == 0) {
+    for (int x = 0; x < ary.length; x++) {
+      if (ary[x].length == 0) {
         tempOutput += "], [";
       }
-      for (int y = 0; y < arry[x].length; y++) {
-        if (y == arry[x].length-1) {
-          tempOutput += arry[x][y] + "], [";
+      for (int y = 0; y < ary[x].length; y++) {
+        if (y == ary[x].length-1) {
+          tempOutput += ary[x][y] + "], [";
         }
         else {
-          tempOutput += arry[x][y] + ", ";
+          tempOutput += ary[x][y] + ", ";
         }
       }
-      if (x == arry.length-1) {
+      if (x == ary.length-1) {
         System.out.print(tempOutput.substring(0, tempOutput.length()-4) + "]]\n");
       }
     }
@@ -86,14 +86,25 @@ public class ArrayDemo {
         }
       }
     }
-    print2DArray(vals);
+    printArray(vals);
   }
 
-  public static void fill2DCopy(int[][] vals){ //2b. Make a new 2d array the same dimensions as the given parameter. Fill in the same way as the previous problem (1s and 3s)
+  public static int[][] fill2DCopy(int[][] vals){ //2b. Make a new 2d array the same dimensions as the given parameter.
     int[][] output = new int[vals.length][];
     for (int x = 0; x < vals.length; x++) {
       output[x] = new int[vals[x].length];
     }
-    fill2D(output);
+    for (int x = 0; x < output.length; x++) {
+      for (int y = 0; y < output[x].length; y++) {
+        if (vals[x][y] >= 0) {
+          output[x][y] = 1;
+        }
+        else {
+          output[x][y] = 3;
+        }
+      }
+    }
+    printArray(output);
+    return output;
   }
 }
